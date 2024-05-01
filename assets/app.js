3e940a9e2079d40c98efe51e51ceda363a1ceed1
DamienOrecchia38 <damien.orecchia.simplon@gmail.com>
2024-05-01 18:07:25 +0200

    Fix - particles animation in JS file

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,102 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('submit', confirmPurchase);
     });
 
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 800,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#ffd700"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": { 
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                }
+            },
+            "opacity": {
+                "value": 0.8,
+                "random": true,
+                "anim": {
+                    "enable": true,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": true,
+                    "speed": 10,
+                    "size_min": 0.1,
+                    "sync": false 
+                }
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none", 
+                "random": true,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false, 
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "bubble"
+                },
+                "onclick": {
+                    "enable": false,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "bubble": {
+                    "distance": 400,
+                    "size": 4,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                }
+            }
+        },
+        "retina_detect": true
+    });
+
+    const particle = document.getElementsByClassName('particle');
+    const particlesDiv = document.getElementById('particles-js');
+    particle.addEventListener('mouseover', function(event) {
+        particlesDiv.style.top = `${event.clientY - 50}px`;
+        particlesDiv.style.left = `${event.clientX - 50}px`;
+    });
+
     const signupButton = document.querySelector('.signup-button');
     if (signupButton) {
         signupButton.addEventListener('click', function(event) {
+            
             event.preventDefault();
+
             swal({
                 title: "Confirmez votre inscription",
                 text: "Êtes-vous sûr de vouloir vous inscrire ?",
@@ -52,10 +144,3 @@ function confirmPurchase(event) {
         }
     });
 }
-
-
-$('.carousel').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
